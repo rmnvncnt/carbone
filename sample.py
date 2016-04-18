a2c6f2165dafc8f2ba24986f3dff1738d8878a2c
syan3 <sarah.yanicostas@gmail.com>
2016-04-18 19:05:57 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,3 +2,5 @@ from carbone.func import *
 product = get_product_infos(7291647867788)
 distance = get_distance_infos(product)
 print distance
+
+print 'composition :'+str(get_ingredients_list(product))
